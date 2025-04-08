98c016454097895634f37bbf94a0fe8e45210a33
eboateng <Eboatenggraham@gmail.com>
2025-04-09 00:15:54 +0100

    .

--- task_manager.py
+++ task_manager.py
@@ -12,13 +12,21 @@ class TaskManager:
         
     def StatusComplete(self):
         self.status = "completed"
+        
+    def __str__(self):
+        return f"Task: {self.name}\nDue: {self.due_date}\nReminder: {self.reminder_time}\nPriority: {self.priority}\nStatus: {self.status}\nDescription: {self.description}"
     
     class TaskMethods:
         def __init__(self):
-            TaskList = []
+            self.TaskList = []
             
         def AddTask(self, name, due_date, reminder_time, description, priority, status):
             NewTask = TaskManager(name,due_date,reminder_time,description,priority,status)
-            self.TaskList = TaskList.Append(NewTask)
+            self.TaskList = self.TaskList.Append(NewTask)
         
-        
+        def DeleteTask(self,name):
+            for Task in self.TaskList:
+                if Task.name == name:
+                    self.TaskList.remove(Task)
+                    break
+                
